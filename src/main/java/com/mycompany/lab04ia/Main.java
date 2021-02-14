597d7d6ce9fb2776f3057201fbb7c7bc36a0b4ab
genecc15 <36336252+genecc15@users.noreply.github.com>
2021-02-14 17:46:22 -0600

    Git Ignore

--- src/main/java/com/mycompany/lab04ia/Main.java
+++ src/main/java/com/mycompany/lab04ia/Main.java
@@ -17,8 +17,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
-        System.out.println("Probando");
-        System.err.println("er");
     }
     
 }
